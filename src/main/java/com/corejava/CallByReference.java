9f953d83a24d6d502c35aa6a64cf760d15a8d2d4
VimalVDS <48423158+VimalVDS@users.noreply.github.com>
2019-05-06 04:53:38 +0530

    Update CallByReference.java

--- src/main/java/com/corejava/CallByReference.java
+++ src/main/java/com/corejava/CallByReference.java
@@ -8,7 +8,7 @@ public class CallByReference {
 	public static void main(String[] args) {
 		
 		Employee e1=new Employee(101,50000.00,"baskar","kukatpally");
-		
+		// testing github operations
 		List<Employee> list=new LinkedList<Employee>();
 		list.add(e1);
 		e1.setEmpName("kosmik");
